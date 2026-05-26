f1d8d3de9b99d1feeac1f6aa214894e1735d8fa7
khanzada44 <minhamkhanzada4455@gmail.com>
2026-05-26 22:49:04 +0500

    first commit

---
+++ prsswordGenerator/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useCallback } from 'react'
+import './App.css'
+
+
+
+function App() {
+  const [lenght, setLenght] = useState(8);
+  const [numberAllow, setNumberAllow] = useState(false);
+  const [cahrAllow, setCahrAllow] = useState(false);
+  const [password, setPassword] = useState('');
+
+  const passwordGenater = useCallback(() => {
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    if (numberAllow) {
+      str += "0123456789"
+    }
+    if (cahrAllow) {
+      str += "!@#$%^&*()_+><:{}[]"
+    }
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char)
+    }
+    setPassword(pass)
+  }, [lenght, numberAllow, cahrAllow, setPassword])
+
+  return (
+    <>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-black'>
+        <h1 className='text-black text-center'>Password Genater</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input type="text" value={password}
+            placeholder='Password'
+            readOnly
+            className='outline-none w-full py-1 px-3' />
+            <button className='outline-none bg-amber-950 text-white px-3 py-0.5 stroke-0'>Copy</button>
+        </div>
+        <div className='flex text-sm gap-x-2 '>
+          <div className='flex items-center gap-x-1'>
+            <input type="range"
+            min={6}
+            max={50}s
+            value={lenght}
+            className='cursor-pointer '
+            onChange={(e) =>{setLenght(e.target.value)}} />
+            <label > Lenght : {lenght}</label>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
