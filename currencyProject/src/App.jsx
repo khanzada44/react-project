494a4620edc2807f6b08de49efa8885f098583f6
khanzada44 <minhamkhanzada4455@gmail.com>
2026-06-04 15:22:19 +0500

    update

---
+++ currencyProject/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-black">
+        <h1 className="text-amber-500">Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
