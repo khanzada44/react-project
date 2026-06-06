97c28eb1acbd7b1ef91d3aed2155cf1202a950d8
khanzada44 <minhamkhanzada4455@gmail.com>
2026-06-06 21:44:57 +0500

    update

--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -1,37 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements,} from "react-router";
 import Layout from "./Layout";
 import Home from "./components/Home/home";
 import About from "./components/About/about";
 import Contact from "./components/Contact/contact";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       {
-//         path: "",
-//         element: <Home />,
-//       },
-//       {
-//         path: "about",
-//         element: <About />,
-//       },
-//       {
-//         path: "contact",
-//         element: <Contact />,
-//       },
-//     ],
-//   },
-// ]);
 
 const router = createBrowserRouter(
   createRoutesFromElements(
