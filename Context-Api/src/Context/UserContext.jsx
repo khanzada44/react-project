05d47c09a0365b9df91ee3ec0415ff5d4258aa1f
khanzada44 <minhamkhanzada4455@gmail.com>
2026-06-14 00:53:00 +0500

    Practices-context-api

---
+++ Context-Api/src/Context/UserContext.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import UserContext from "./Context/UserContext";
+
+const UserContexProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return (
+    <UserContext.Provider vaule={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContexProvider;
